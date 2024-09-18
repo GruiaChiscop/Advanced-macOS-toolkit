4dd5b231120843bee370167f10cbb347449133ce
Gruia Chiscop <chiscopgruia@gmail.com>
2024-09-18 10:20:02 +0300

    Initial Commit

---
+++ Advanced macOS toolkit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Advanced macOS toolkit
+//
+//  Created by Gruia Chiscop on 9/18/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
